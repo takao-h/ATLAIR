abf4edfd4d0b760451e35e7bcaff86d6ed4a054c
takao-h <hayashi_takao@cyberbuzz.co.jp>
2019-04-28 04:45:52 +0900

    fix lint & diff load

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var app = express();
 var port = 3000;
-app.get('/', function (req, res) { return res.send('Hello World!'); });
-app.listen(port, function () { return console.log("Example app listening on port " + port + "!"); });
+app.get('/', function (req, res) { return res.send('Hello World!!!!'); });
+app.listen(port, function () { return console.log("\uD83D\uDE80 Example app listening on port " + port + "!"); });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import * as express from "express";
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!!!!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`🚀 Example app listening on port ${port}!`));
